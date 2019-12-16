88165832b14d31516486485dbd9531acbf4a1c27
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-12-16 15:14:36 -0200

    Configurando Root Import

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,16 +28,25 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      { extensions: ['.jsx', '.js']}
-    ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
-    'react/state-in-constructor': 'off',
-    'react/static-property-placement': 'off',
-    'no-console': ["error", { allow: ["tron"] }],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
     'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'camelcase': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src'
+      }
+    }
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
-  import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f5fcff'
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: '600',
-      margin: 10,
-      textAlign: 'center'
-    }
-  });
-
-  function App() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello World</Text>
-      </View>
-    );
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    margin: 10,
+    textAlign: 'center'
   }
+});
 
-  export default App;
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+    </View>
+  );
+}
+
+export default App;
