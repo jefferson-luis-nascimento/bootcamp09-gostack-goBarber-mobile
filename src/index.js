a7b860070352788cc6107502eb8756caee410a1a
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-12-16 15:27:41 -0200

    Rotas de autenticação

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff'
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: '600',
-    margin: 10,
-    textAlign: 'center'
-  }
-});
+import Routes from './routes';
 
 function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-    </View>
-  );
+  return <Routes />;
 }
 
 export default App;
