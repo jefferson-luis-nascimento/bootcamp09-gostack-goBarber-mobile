658c661cd8cb64ae1422bdeaaefb2d9d4496138b
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-12-16 18:14:39 -0200

    Configurando Redux

---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,33 @@
+import { Alert } from 'react-native';
+
+import { takeLatest, put, call, all } from 'redux-saga/effects';
+
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailiure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    Alert.alert('Sucesso', 'Perfil atualizado com sucesso.');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    Alert.alert(
+      'Falha na atualização',
+      'Houve um erro na atualização do perfil. Confira seus dados'
+    );
+    yield put(updateProfileFailiure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
